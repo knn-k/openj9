4f98a41de63db356c41730489bd8fa42e2ce1774
KONNO Kazuhiro <konno@jp.ibm.com>
2026-04-13 16:44:07 +0900

    Recognize and transform StringLatin1.compareTo()
    Signed-off-by: KONNO Kazuhiro <konno@jp.ibm.com>

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -5459,10 +5459,16 @@ bool TR_J9InlinerPolicy::suppressInliningRecognizedInitialCallee(TR_CallSite *ca
             }
             break;
         }
+        case TR::java_lang_StringLatin1_compareTo_BBII:
+            if (comp->cg()->getSupportsArrayCmpLen()) {
+                return true;
+            }
+            break;
         case TR::java_lang_StringLatin1_inflate_BICII:
             if (cg->getSupportsInlineStringLatin1Inflate()) {
                 return true;
             }
+            break;
         case TR::java_lang_StringLatin1_inflate_BIBII:
             if (cg->getSupportsArrayTranslateTROTNoBreak() && !comp->target().cpu.isPower()) {
                 return true;
